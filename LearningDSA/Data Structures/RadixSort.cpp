4306fbb14760ce2d442ad43279ca28f324cc0fec
Kerem Semiz <kerox350@gmail.com>
2024-08-11 16:19:17 +0300

    Made a RadixSort system

---
+++ LearningDSA/Data Structures/RadixSort.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int getMax(const std::vector<int>& arr) {
+    return *max_element(arr.begin(), arr.end());
+}
+
+void countSort(std::vector<int>& arr, int exp) {
+    int n = arr.size();
+    std::vector<int> output(n);
+    std::vector<int> count(10, 0);
+
+    for (int i = 0; i < n; i++) {
+        count[(arr[i] / exp) % 10]++;
+    }
+
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void radixSort(std::vector<int>& arr) {
+    int max = getMax(arr);
+
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        countSort(arr, exp);
+    }
+}
+
+int main() {
+    std::vector<int> arr = {170, 45, 75, 90, 802, 24, 2, 66};
+    std::cout << "Given array: ";
+    for (int num : arr)
+        std::cout << num << " ";
+    std::cout << std::endl;
+
+    radixSort(arr);
+
+    std::cout << "Sorted array: ";
+    for (int num : arr)
+        std::cout << num << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
